fb50ae0529f211375634e57f6d0fdcff93853633
Patrick Almeida <patricksalmeida@gmail.com>
2023-04-21 16:16:04 -0300

    feat: add account entity and relationship with client

---
+++ internal/entity/account_test.go
@@ -0,0 +1,26 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAccount(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "jhon@doe.com")
+	account := NewAccount(client)
+	assert.NotNil(t, account)
+	assert.Equal(t, client.ID, account.Client.ID)
+}
+
+func TestCreateAccountWithNilClient(t *testing.T) {
+	account := NewAccount(nil)
+	assert.Nil(t, account)
+}
+
+func TestCreditAccount(t *testing.T) {
+	client, _ := NewClient("Jhon Doe", "jhon@doe.com")
+	account := NewAccount(client)
+	account.Credit(100)
+	assert.Equal(t, float64(100), account.Balance)
+}
