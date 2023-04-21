3cb955521301c09599fd9ac6a7fe484adb229968
Patrick Almeida <patricksalmeida@gmail.com>
2023-04-21 16:51:19 -0300

    feat: add create client use case

---
+++ internal/usecase/create_client/create_client_test.go
@@ -0,0 +1,44 @@
+package createclient
+
+import (
+	"testing"
+
+	"github.com.br/patricksalmeida/fc3-digitalwallet/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type ClientGatewayMock struct {
+	mock.Mock
+}
+
+func (m *ClientGatewayMock) Save(client *entity.Client) error {
+	args := m.Called(client)
+	return args.Error(0)
+}
+
+func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Client), args.Error(1)
+}
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	m := &ClientGatewayMock{}
+	m.On("Save", mock.Anything).Return(nil)
+
+	uc := NewCreateClientUseCase(m)
+
+	output, err := uc.Execute(CreateClientInputDTO{
+		Name:  "Jhon Doe",
+		Email: "jhon@doe.com",
+	})
+
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	assert.Equal(t, "Jhon Doe", output.Name)
+	assert.Equal(t, "jhon@doe.com", output.Email)
+
+	m.AssertExpectations(t)
+	m.AssertNumberOfCalls(t, "Save", 1)
+}
