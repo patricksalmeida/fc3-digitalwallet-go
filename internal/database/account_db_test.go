9f9efb44a3151acda0df99af00d8517f8a04c7ae
Patrick Almeida <patricksalmeida@gmail.com>
2023-04-21 19:12:51 -0300

    feat: add account db and test suite

---
+++ internal/database/account_db_test.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+
+	"github.com.br/patricksalmeida/fc3-digitalwallet/internal/entity"
+	"github.com/stretchr/testify/suite"
+)
+
+type AccountDBTestSuite struct {
+	suite.Suite
+	db        *sql.DB
+	accountDB *AccountDB
+	client    *entity.Client
+}
+
+func (s *AccountDBTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	s.Nil(err)
+	s.db = db
+	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
+	db.Exec("CREATE TABLE accounts (id string, client_id string, balance float, created_at date)")
+	s.accountDB = NewAccountDB(db)
+	s.client, _ = entity.NewClient("Jhon Doe", "jhon@doe.com")
+}
+
+func (s *AccountDBTestSuite) TearDownSuite() {
+	defer s.db.Close()
+	s.db.Exec("DROP TABLE accounts")
+	s.db.Exec("DROP TABLE clients")
+}
